b5ace7f9be74a2707b30445564901fd2e6a59cec
drabinowitz <dmitrirabinowitz+development@gmail.com>
2014-06-20 17:08:59 -0400

    goHome

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
-
 $(document).ready(function(){
-	
+
+	var newGame100 = newGame(100);
+
+	newGame100.resetGame();
+
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
@@ -14,4 +17,51 @@ $(document).ready(function(){
 
 });
 
+var newGame = function(val){
 
+	var secretNumber;
+
+	return {
+
+		returnNum: function(){
+
+			return secretNumber;
+
+		},
+
+		resetGame: function(){
+
+			secretNumber = Math.ceil(Math.random()*val);
+
+			$('#count').text() = 0;
+
+			$('#feedback').text() = "Make your Guess!";
+
+		}
+
+	}
+
+};
+
+
+var feedback = function(input,secretNumber){
+
+	var checkVal = function(){
+
+
+	
+	}
+
+	var prevVal = +$('#feedback');
+
+	if ( !(input - secretNumber) ){
+
+		alert('You Won!');
+
+	} else if( prevVal ){
+
+
+
+	}
+
+}
